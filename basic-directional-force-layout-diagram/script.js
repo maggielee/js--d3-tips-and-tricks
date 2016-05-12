3756446ddf48b33199ccd6e61e41e93041497af4
Vasil Vanchuk <v.vanchuk@tut.by>
2016-05-12 22:56:36 +0300

    Basic directional layout diagram

---
+++ basic-directional-force-layout-diagram/script.js
@@ -0,0 +1,80 @@
+const width = 960;
+const height = 500;
+
+const svg = d3.select("#container")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+d3.csv("data.csv", (error, links) => {
+  var nodes = {};
+
+  // Compute the distinct nodes from the links.
+  links.forEach((link) => {
+    link.source = nodes[link.source] || (nodes[link.source] = { name: link.source });
+    link.target = nodes[link.target] || (nodes[link.target] = { name: link.target });
+    link.value = +link.value;
+  });
+
+  const force = d3.layout.force()
+    .nodes(d3.values(nodes))
+    .links(links)
+    .size([width, height])
+    .linkDistance(60)
+    .charge(-300)
+    .on("tick", tick)
+    .start();
+
+
+  // build the arrow.
+  svg.append("svg:defs").selectAll("marker")
+    .data(["end"])      // Different link/path types can be defined here
+    .enter().append("svg:marker")    // This section adds in the arrows
+    .attr("id", String)
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 15)
+    .attr("refY", -1.5)
+    .attr("markerWidth", 6)
+    .attr("markerHeight", 6)
+    .attr("orient", "auto")
+    .append("svg:path")
+    .attr("d", "M0,-5L10,0L0,5");
+
+  // add the links and the arrows
+  const path = svg.append("svg:g").selectAll("path")
+    .data(force.links())
+    .enter().append("svg:path")
+    .attr("class", (d) => `link ${d.type}`)
+    .attr("class", "link")
+    .attr("marker-end", "url(#end)");
+
+  // define the nodes
+  const node = svg.selectAll(".node")
+    .data(force.nodes())
+    .enter().append("g")
+    .attr("class", "node")
+    .call(force.drag);
+
+  // add the nodes
+  node.append("circle")
+    .attr("r", 5);
+
+  // add the text
+  node.append("text")
+    .attr("x", 12)
+    .attr("dy", ".35em")
+    .text((d) => d.name);
+
+  // add the curvy lines
+  function tick() {
+    path.attr("d", (d) => {
+      const dx = d.target.x - d.source.x;
+      const dy = d.target.y - d.source.y;
+      const dr = Math.sqrt(dx * dx + dy * dy);
+      return `M${d.source.x},${d.source.y}A${dr},${dr} 0 0,1 ${d.target.x},${d.target.y}`;
+    });
+
+    node.attr("transform", (d) => `translate(${d.x},${d.y})`);
+  }
+
+});
