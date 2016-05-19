614eb39b0b5e537305b6143334ea88cac091ad31
Vasil Vanchuk <v.vanchuk@tut.by>
2016-05-19 11:01:49 +0300

    Map zooming

--- simple-map/script.js
+++ simple-map/script.js
@@ -10,11 +10,8 @@ const svg = d3.select("#container").append("svg")
   .attr("width", width)
   .attr("height", height);
 
-const path = d3.geo.path()
-  .projection(projection);
-
+const path = d3.geo.path().projection(projection);
 const g = svg.append("g");
-
 const c20c = d3.scale.category20c();
 
 // load and display the World
@@ -26,3 +23,10 @@ d3.json("world-110m2.json", (error, topology) => {
     .attr("d", path)
     .style('fill', (d) => c20c(d.id))
 });
+
+var zoom = d3.behavior.zoom()
+  .on("zoom",() => {
+    g.attr("transform",`translate(${d3.event.translate.join(",")})scale(${d3.event.scale})`);
+    g.selectAll("path").attr("d", path.projection(projection));
+  });
+svg.call(zoom);
